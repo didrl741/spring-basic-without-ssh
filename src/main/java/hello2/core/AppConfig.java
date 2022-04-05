8abaf02462e180654ac3391ca326c9233174148f
Yanggi Song <didrl741@gmail.com>
2022-04-05 20:46:14 +0900

    스프링 첫 활용: 스프링 빈과 컨테이너
    AppConfig에 설정을 구성한다는 뜻의 @Configuration 을 붙여준다.
    각 메서드에 @Bean 을 붙여준다. 이렇게 하면 스프링 컨테이너에 스프링 빈으로 등록한다.
    
    ApplicationContext를 스프링 컨테이너라고 한다!
    
    이제부터는 이것을 통해 DI 할 것이다.
    스프링 컨테이너는 @Configuration 이 붙은 AppConfig 를 설정(구성) 정보로 사용한다. 여기서 @Bean
    이라 적힌 메서드를 모두 호출해서 반환된 객체를 스프링 컨테이너에 등록한다.
    이렇게 스프링 컨테이너에 등록된 객체를 스프링 빈이라 한다.
    스프링 빈은 @Bean 이 붙은 메서드의 명을 스프링 빈의 이름으로 사용한다. ( memberService , orderService )
    필요한 스프링 빈은 applicationContext.getBean() 메서드를 사용해서 찾을 수 있다.
    
    코드만 더 복집해진거 아니야? -> 아니다!
    스프링 빈은 엄청한 장점들이 있다. 이제부터 쭉 알아보자.

--- src/main/java/hello2/core/AppConfig.java
+++ src/main/java/hello2/core/AppConfig.java
@@ -9,21 +9,28 @@ import hello2.core.member.MemberServiceImpl;
 import hello2.core.member.MemoryMemberRepository;
 import hello2.core.order.OrderService;
 import hello2.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
--- src/main/java/hello2/core/MemberApp.java
+++ src/main/java/hello2/core/MemberApp.java
@@ -4,15 +4,21 @@ import hello2.core.member.Grade;
 import hello2.core.member.Member;
 import hello2.core.member.MemberService;
 import hello2.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 // 클라이언트
 public class MemberApp {
 
     public static void main(String[] args) {
-        //MemberService memberService = new MemberServiceImpl();
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        // 여기서 name: "memberService"은 bean 메서드의 이름.
+
 
         Member member = new Member(1L, "memberA", Grade.VIP);
 
