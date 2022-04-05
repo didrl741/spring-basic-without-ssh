3c0d4242ca8c0416c0c69766c00084936256f4f9
Yanggi Song <didrl741@gmail.com>
2022-04-05 12:34:55 +0900

    AppConfig 리팩토링
    중복을 제거하고, 역할에 따른 구현이 한눈에 들어오도록 하자.
    
    코드 수정 후: 이제 구현체를 바꾸고 싶다면?
    한 줄만 바꾸면 된다.

--- src/main/java/hello2/core/AppConfig.java
+++ src/main/java/hello2/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello2.core;
 
+import hello2.core.discount.DiscountPolicy;
 import hello2.core.discount.FixDiscountPolicy;
+import hello2.core.member.MemberRepository;
 import hello2.core.member.MemberService;
 import hello2.core.member.MemberServiceImpl;
 import hello2.core.member.MemoryMemberRepository;
@@ -9,12 +11,21 @@ import hello2.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
-    // 생성자 주입
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
+
+
 }
