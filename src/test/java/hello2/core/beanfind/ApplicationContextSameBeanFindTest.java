cfe39e07170bbb855c72ba4f378c83252e4b5410
Yanggi Song <didrl741@gmail.com>
2022-04-08 00:28:30 +0900

    스프링 빈 조회 - 동일한 타입이 둘 이상일 때
    타입으로 조회시: 오류.
    이름과 타입으로 조회시: 오류X
    ac.getBeansOfType() 사용시: 빈 객체들이 Map<String, MemberRepository> 형태로 반환됨.

---
+++ src/test/java/hello2/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -0,0 +1,63 @@
+package hello2.core.beanfind;
+
+import hello2.core.member.MemberRepository;
+import hello2.core.member.MemberService;
+import hello2.core.member.MemberServiceImpl;
+import hello2.core.member.MemoryMemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+public class ApplicationContextSameBeanFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
+
+    @Test
+    @DisplayName("타입으로 조회시 같은 타입이 둘 이상이면 중복오류 발생")
+    void findBeanByTypeDuplicate() {
+        //MemberRepository memberRepository = ac.getBean(MemberRepository.class);
+        Assertions.assertThrows(NoUniqueBeanDefinitionException.class,
+                () -> ac.getBean(MemberRepository.class));
+    }
+
+    @Test
+    @DisplayName("같은 타입이 둘 이상이면, 빈 이름을 지정하면 된다.")
+    void findBeanByName() {
+        MemberRepository memberRepository = ac.getBean("memberRepository1", MemberRepository.class);
+        org.assertj.core.api.Assertions.assertThat(memberRepository).isInstanceOf(MemberRepository.class);
+    }
+
+    @Test
+    @DisplayName("특정 타입을 모두 조회")
+    void findAllBeanByType() {
+        Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value = " + beansOfType.get(key));
+        }
+        System.out.println("beansOfType = " + beansOfType);
+        org.assertj.core.api.Assertions.assertThat(beansOfType.size()).isEqualTo(2);
+
+    }
+
+
+
+    // 클래스안의 클래스: 여기서만 쓰겠다는 의미.
+    @Configuration
+    static class SameBeanConfig {
+        @Bean
+        public MemberRepository memberRepository1() {
+            return new MemoryMemberRepository();
+        }
+        @Bean
+        public MemberRepository memberRepository2() {
+            return new MemoryMemberRepository();
+        }
+    }
+
+}
