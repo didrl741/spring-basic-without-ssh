a3c4cd53c825963d3dc584e4aa4ca0200c4d0de9
Yanggi Song <didrl741@gmail.com>
2022-04-08 11:16:40 +0900

    스프링 빈 조회 - 상속관계
    여러 하위클래스가 있는 부모 클래스객체 조회시 - NoUniqueBeanDefinitionException 에러.
    구체적인 하위 클래스의 이름 (name)으로 조회하면 된다.
    또한 getBeansOfType을 이용해 부모타입으로 모두조회도 가능.
    
    만약 Object로 조회하면?
    모든 클래스의 최상위클래스이기 때문에 온갖 빈 객체가 다 튀어나온다.

---
+++ src/test/java/hello2/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -0,0 +1,79 @@
+package hello2.core.beanfind;
+
+import hello2.core.discount.DiscountPolicy;
+import hello2.core.discount.FixDiscountPolicy;
+import hello2.core.discount.RateDiscountPolicy;
+import hello2.core.member.MemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoUniqueBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+
+public class ApplicationContextExtendsFindTest {
+
+    AnnotationConfigApplicationContext ac =
+            new AnnotationConfigApplicationContext(ApplicationContextExtendsFindTest.Testconfig.class);
+
+
+    @Test
+    @DisplayName("부모타입으로 조회시, 자식이 둘 이상 있으면 중복오류 발생")
+    void findBeanByTypeDuplicate() {
+        //DiscountPolicy discountPolicy = ac.getBean(DiscountPolicy.class);
+        Assertions.assertThrows(NoUniqueBeanDefinitionException.class,
+                () -> ac.getBean(DiscountPolicy.class));
+    }
+
+    @Test
+    @DisplayName("부모타입으로 조회시, 자식이 둘 이상 있으면 빈 이름을 지정하면 된다")
+    void findBeanByParentTypeBeanName() {
+        DiscountPolicy discountPolicy = ac.getBean("rateDiscountPolicy", RateDiscountPolicy.class);
+        org.assertj.core.api.Assertions.assertThat(discountPolicy).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("특정 하위 타입으로 조회")   // 안좋다. 추상화가 좋다.
+    void findBeanBySubType() {
+        RateDiscountPolicy bean = ac.getBean(RateDiscountPolicy.class);
+        org.assertj.core.api.Assertions.assertThat(bean).isInstanceOf(RateDiscountPolicy.class);
+    }
+
+    @Test
+    @DisplayName("부모타입으로 모두 조회")
+    void findAllBeanByParentType() {
+        Map<String, DiscountPolicy> beansOfType = ac.getBeansOfType(DiscountPolicy.class);
+        org.assertj.core.api.Assertions.assertThat(beansOfType.size()).isEqualTo(2);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value = " + beansOfType.get(key));
+        }
+    }
+
+    @Test
+    @DisplayName("부모타입으로 모두 조회 - Object: 모든 클래스의 부모")
+    void findAllBeanByObjectType() {
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value = " + beansOfType.get(key));
+        }
+    }
+
+    @Configuration
+    static class Testconfig {
+
+        @Bean
+        public DiscountPolicy rateDiscountPolicy() {
+            return new RateDiscountPolicy();
+        }
+
+        @Bean
+        public DiscountPolicy fixDiscountPolicy() {
+            return new FixDiscountPolicy();
+        }
+    }
+
+}
