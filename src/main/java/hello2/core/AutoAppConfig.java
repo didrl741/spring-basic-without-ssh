083bf7187cb2cbdde6689f7fa6dee3bc76f7a4f3
Yanggi Song <didrl741@gmail.com>
2022-04-15 16:11:39 +0900

    중복 등록과 충돌
    자동 빈 등록vs자동 빈 등록 : 오류 발생.
    
    수동vs자동 : 스프링에서는 수동 빈이 자동 빈을 오버라이딩 해서 수동빈이 우선등록.
    하지만, 스프링 부트인 CoreApplication을 실행해보면 오류가 뜬다.
    즉, 스프링 부트에서는 이것을 허용하지 않는다.
    
    실무에서는 이런 애매한 버그가 잡기 어렵기때문에 아예 막아버린 것.
    (여러사람과 협업하다보면 의도와 상관없이 버그가 나오기 쉽다)

--- src/main/java/hello2/core/AutoAppConfig.java
+++ src/main/java/hello2/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello2.core;
 
+import hello2.core.member.MemberRepository;
+import hello2.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,4 +15,9 @@ import static org.springframework.context.annotation.ComponentScan.*;
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class AutoAppConfig {
     // AppConfig와 충돌 피하려고 exclude 함. 보통은 안함.
+
+//    @Bean(name="memoryMemberRepository")
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
