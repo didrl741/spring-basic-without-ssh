64caf42cfd8acef8f167b9ba023c28b44c726462
Yanggi Song <didrl741@gmail.com>
2022-04-29 11:32:38 +0900

    DL (Dependency Lookup: 필요한 의존관계를 직접 찾는 것)
    방법1: ac를 싱글톤 빈의 필드에 DI로 넣고 프로토타입 빈 조회할 때마다 getBean 하는것
    : 무식하고 안좋은 방법이다.
    
    방법2: ObjectProvider 사용
    : 편하다, 스프링이 제공하는 기능을 사용하지만, 기능이 단순하므로 단위테스트를 만들거나 mock 코드를 만들기는 훨씬 쉬워진다. 지금 딱 필요한 DL 정도의 기능만 제공한다.
    ObjectFactory를 상속한다.
    
    방법3: 자바 표준의 Provider
    : gradle에 추가해야 한다, 자바 표준이므로 스프링이 아닌 다른 컨테이너에서도 사용할 수 있다, 단순하다.
    
    정리:
    프로토타입 빈 언제사용? -> 매번 사용할 때마다 의존관계 주입이 완료된 새로운 객체가 필요할 때 사용. 그런데 실무에서는 대부분 싱글톤으로 해결할 수 있다.
    ObjectProvider는 프로토타입 뿐 아니라 DL이 필요할 때 언제든지 사용 가능.
    왠만하면 ObjectProvider 쓰자.

---
+++ src/test/java/scope/PrototypeProviderTest.java
@@ -0,0 +1,63 @@
+package scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class PrototypeProviderTest {
+
+    @Test
+    void prototypeBeanTest() {
+
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class, PrototypeBean.class);
+
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+
+        System.out.println(singletonBean1.logic());
+        System.out.println(singletonBean2.logic());
+
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+
+        @Autowired
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init " + this);
+        }
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
