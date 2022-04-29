1714ddba5ef43f0c8603c25a8d4c07cd269aba38
Yanggi Song <didrl741@gmail.com>
2022-04-29 10:47:42 +0900

    싱글톤에서 프로토타입 빈 사용
    싱글톤 빈이 의존관계 주입을 통해서 프로토타입 빈을 주입받아서 사용하는 예 이다.
    서로 다른 클라이언트가 싱글톤 빈을 조회하면 서로 다른 프로토타입 빈을 사용하게 될까?
    -> 아니다. 의존관계 주입 시점에 이미 프로토타입 빈을 주입받아서 필드로 가지고 있고,
    서로 다른 클라이언트라도 싱글톤빈이기 때문에 같은 인스턴스를 조회받게 된다.
    
    즉,  싱글톤 빈이 내부에 가지고 있는 프로토타입 빈은 이미 과거에 주입이 끝난
    빈이다. 주입 시점에 스프링 컨테이너에 요청해서 프로토타입 빈이 새로 생성이 된 것이지, 사용 할 때마다
    새로 생성되는 것이 아니다.
    
    우리가 원했던 것: 프로토타입 빈을 주입시점에만 새로 생성하는게 아니라
    사용할 때 마다 새로 생성하는 것. (서로 다른 클라이언트가)

---
+++ src/test/java/scope/SingletonWithPrototypeTest.java
@@ -0,0 +1,65 @@
+package scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonWithPrototypeTest {
+
+    @Test
+    void prototypeBeanTest() {
+
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class, PrototypeBean.class);
+
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+
+        System.out.println(singletonBean1.logic());
+        System.out.println(singletonBean2.logic());
+
+        Assertions.assertThat(singletonBean1.logic()).isNotSameAs(singletonBean2.logic());
+
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+        private final PrototypeBean prototypeBean;
+
+        @Autowired
+        public SingletonBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init " + this);
+        }
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
